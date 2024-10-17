5d44bc5e5533f2e83635173bc31035502a78b0a2
JorgeDDuran <jorgerena3@gmail.com>
2024-10-17 14:09:41 -0500

    conectado a modulos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+//App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './views/Dashboard';
 import DataDictionary from './views/DataDictionary';
 import DDL from './views/DDL';
 import DML from './views/DML';
+import NotFound from './views/NotFound'; // Asegúrate de tener este componente creado
 import Tutorial from './views/Tutorial'; // Asegúrate de que la ruta sea correcta
 import Sidebar from './components/Sidebar'; // Sidebar
 import './assets/App.css'; // Importa tu CSS
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 
 const App = () => {
   return (
@@ -28,6 +31,7 @@ const App = () => {
               <Route path="/data-dictionary" element={<DataDictionary />} />
               <Route path="/ddl" element={<DDL />} />
               <Route path="/dml" element={<DML />} />
+              <Route path="*" element={<NotFound />} /> {/* Ruta para manejar 404 */}
             </Routes>
           </section>
         </div>
