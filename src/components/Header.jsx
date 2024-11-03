e2748d6ed5a16572509c044417ec0ec82f0cbf7a
JorgeDDuran <jorgerena3@gmail.com>
2024-11-03 18:04:05 -0500

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import logo from '../assets/Imagenes/Escudo_Universidad_de_Cundinamarca.webp'; // Asegúrate de usar la ruta correcta de tu imagen
 
 function Header() {
   return (
     <header className="header">
+      <img src={logo} alt="Logo" className="header-logo" />
       <h1>Prototipo Sistema gestor de bases de datos</h1>
     </header>
   );
 }
 
 export default Header;
+
